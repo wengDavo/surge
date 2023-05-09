351181775c26e03dd7341825164e905ac44157b1
weng davo <immanueldavo@example.com>
2023-05-09 14:54:31 +0100

    turned of debug mode

--- my_site/settings.py
+++ my_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env('DEBUG')
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
